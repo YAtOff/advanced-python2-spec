b4055086731a7cc9871b1d73680108120519c1a4
Yavor Atov <yavor.atov@gmail.com>
2014-01-25 16:38:13 +0000

    Added some doctests

--- about_metaclasses.py
+++ about_metaclasses.py
@@ -1,17 +1,6 @@
 """
-1. Metaclasses create classes.
-2. Classes create objects.
-3. Classes are objects.
-=> Metaclasses are classes' classes.
-
-Everything is an object in Python, and they are all either instances of
-classes or instances of metaclasses.
-
-Except for `type`.
-
-`type` is actually its own metaclass. This is not something you could
-reproduce in pure Python, and is done by cheating a little bit at the
-implementation level.
+Befor dealing with metaclasses, we should know more about `type`.
+See `describe_type` first.
 
 A metaclass is any callable that takes paremeters for:
     - the class name
@@ -21,17 +10,43 @@ A metaclass is any callable that takes paremeters for:
 `type` can do that too.
 `type` is the default metaclass.
 
-Since `type` is a class, a metaclass could be created by subclassing type
-(see about_metaclasses_example.py).
+1. Metaclasses create classes.
 
-Before considering to use metaclasses read that:
+>>> NewType = type('NewType', (object,), {'answer': 42})
+>>> n = NewType()
+>>> n.answer
+42
 
-        Metaclasses are deeper magic than 99% of users should ever worry
-        about. If you wonder whether you need them, you don't (the people
-        who actually need them know with certainty that they need them,
-        and don't need an explanation about why).
+2. Classes create objects.
 
-        Python Guru Tim Peters
+>>> n = NewType()
+>>> isinstance(n, object)
+True
+
+3. Classes are objects.
+
+>>> isinstance(NewType, object)
+True
+
+=> Metaclasses are classes' classes.
+
+>>> class MyMetaclass(type): pass
+>>> class MyClass(object):
+...     __metaclass__ = MyMetaclass
+>>> MyClass.__class__.__name__
+'MyMetaclass'
+
+Note: Since `type` is a class, a metaclass could be created by subclassing
+`type`.
+
+Everything is an object in Python, and they are all either instances of
+classes or instances of metaclasses.
+
+Except for `type`.
+
+`type` is actually its own metaclass. This is not something you could
+reproduce in pure Python, and is done by cheating a little bit at the
+implementation level.
 """
 
 from expecter import expect
@@ -56,3 +71,17 @@ class describe_type:
 
     def it_is_the_type_a_class(self):
         expect(type(list)) == type
+
+    def it_is_the_type_of_itself(self):
+        expect(type(type)) == type
+
+"""
+Before considering to use metaclasses read that:
+
+        Metaclasses are deeper magic than 99% of users should ever worry
+        about. If you wonder whether you need them, you don't (the people
+        who actually need them know with certainty that they need them,
+        and don't need an explanation about why).
+
+        Python Guru Tim Peters
+"""
