ce9b1763bf17c7d329c34671d4e7f3f1dd670a0f
Yavor Atov <yavor.atov@gmail.com>
2014-01-26 20:33:21 +0000

    Remove buggy C3 aglorithm implementation

--- about_mro.py
+++ about_mro.py
@@ -23,7 +23,6 @@ Level 1            1 | B |    | C | 2                 |
 Level 0                 0 | A |                (more specialized)
                            ---
 
-source: http://www.python.org/download/releases/2.3/mro/
 
 Whats is the MRO of clase `A`?
 
@@ -34,8 +33,8 @@ Whats is the MRO of clase `A`?
 
 But where that comes from?
 MRO is calculated with the C3 algorithm.
-To understand the C3 algotithm, we will implement it in Python.
-The algorithm is ispired from: http://www.python.org/download/releases/2.3/mro/
+To understand the C3 algotithm, implement one.
+Or see the implementation on https://github.com/mikeboers/C3Linearize.git
 """
 
 O = object
@@ -45,78 +44,3 @@ class D(O): pass
 class C(D,F): pass
 class B(D,E): pass
 class A(B,C): pass
-
-
-from collections import deque
-from expecter import expect
-
-
-def C3(cls, herarchy):
-    """
-        * `cls` is the name or the class, whose MRO we are searching
-        * `herarchy` is in the form:
-            {
-                class_name: [..., base_class_name, ...]
-                ...
-            }
-          but we will refer to it as head -> tail
-    """
-
-    def get_all_classes(classes):
-        if not classes:
-            return []
-        superclasses = [sc for c in classes for sc in get_all_classes(herarchy[c])]
-        return classes + superclasses
-
-    def is_good(head):
-        tails = sum((get_all_classes(herarchy[p]) for p in pending), [])
-        return all(head not in tail for tail in tails)
-
-    mro = [cls]
-    bases = herarchy[cls]
-    pending = deque(bases)
-    while pending:
-        head = pending.popleft()
-        if is_good(head):
-            mro.append(head)
-            tail =  herarchy[head]
-            if tail:
-                pending.appendleft(tail[0])
-                pending.extend(t for t in tail[1:] if is_good(t))
-
-    return mro
-
-
-class describe_mro:
-
-    def it_is_calculated_by_c3(self):
-        expected_mro = [c.__name__ for c in A.mro()]
-        expect(C3('A', {
-            'A': ['B', 'C'],
-            'B': ['D', 'E'],
-            'C': ['D', 'F'],
-            'D': ['object',],
-            'E': ['object',],
-            'F': ['object',],
-            'object': []
-        })) == expected_mro
-
-    def it_works_with_another_herarchy(self):
-        O = object
-        class F(O): pass
-        class E(O): pass
-        class D(O): pass
-        class C(D,F): pass
-        class B(E,D): pass
-        class A(B,C): pass
-
-        expected_mro = [c.__name__ for c in A.mro()]
-        expect(C3('A', {
-            'A': ['B', 'C'],
-            'B': ['E', 'D'],
-            'C': ['D', 'F'],
-            'D': ['object',],
-            'E': ['object',],
-            'F': ['object',],
-            'object': []
-        })) == expected_mro
